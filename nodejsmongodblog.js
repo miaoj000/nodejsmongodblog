cf8d4a4e9daa6192172c00e0ff0896037dd9f102
miaojj <1712446101@qq.com>
2021-12-12 17:38:54 -0800

    1213

--- nodejsmongodblog.js
+++ nodejsmongodblog.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://172.21.2.236/190110910520')
+const ejs = require('ejs')
+// mongoose.connect('mongodb://172.21.2.236/190110910520')
+mongoose.connect('mongodb://localhost:27017/test1')
 const schema = {first:Number, second:Number}
 const nums = mongoose.model('nums', schema)
 app.use('/',express.static('public'))
@@ -16,6 +18,10 @@ app.get('/input', function (req, res) {
     num2 = parseFloat(req.query.num2)
     var num = new nums({first:num1,second:num2})
     num.save()
+    ejs.renderFile('result.html',{returnVal:"success"},(err,str)=>{
+        // console.log(err)
+        res.send(str)
+    })
 })
 
 app.listen(10520)
